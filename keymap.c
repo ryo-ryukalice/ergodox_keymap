9207f6d653e7395a5b8382aeafabb70afc10f30b
ogihara-ryo <r-ogihara@froide-kk.co.jp>
2017-02-07 21:08:17 +0900

    Refacter

--- keymap.c
+++ keymap.c
@@ -169,10 +169,10 @@ void matrix_scan_user(void) {
     ergodox_right_led_3_off();
     switch (layer) {
         // TODO: Make this relevant to the ErgoDox EZ.
-        case 1:
+        case SYMB:
             ergodox_right_led_1_on();
             break;
-        case 2:
+        case MDIA:
             ergodox_right_led_2_on();
             break;
         default:
