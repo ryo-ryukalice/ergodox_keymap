be7e68d0036184b6a36e04bb39dcfd4172ef515c
ogihara-ryo <r-ogihara@froide-kk.co.jp>
2017-02-07 20:26:44 +0900

    Layout

--- keymap.c
+++ keymap.c
@@ -84,7 +84,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
                                KC_TRNS,KC_TRNS,KC_TRNS,
        // right hand
        KC_TRNS, KC_F6,   KC_F7,  KC_F8,   KC_F9,   KC_F10,  KC_F11,
-       KC_TRNS, KC_LABK,   KC_7,   KC_8,    KC_9,    KC_ASTR, KC_F12,
+       KC_TRNS, KC_LABK, KC_7,   KC_8,    KC_9,    KC_ASTR, KC_F12,
                 KC_RABK, KC_4,   KC_5,    KC_6,    KC_PLUS, KC_EQL,
        KC_TRNS, KC_AMPR, KC_1,   KC_2,    KC_3,    KC_BSLS, KC_TRNS,
                          KC_TRNS,KC_DOT,  KC_0,    KC_EQL,  KC_TRNS,
